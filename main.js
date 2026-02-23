29bdb80f2c2bae8a5bcb71608f0b6778e25ca62a
gialli24 <a.gialli2409@gmail.com>
2026-02-23 16:44:35 +0100

    Update for 10 emails

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
 /* Endpoint */
 const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
 
-/* API Call using fetch API  */
-fetch(endpoint)
-    .then(response => response.json())
-    .then(data => {
-        /* Store Data */
-        console.log(data);
+function addEmail() {
+    /* API Call using fetch API  */
+    fetch(endpoint)
+        .then(response => response.json())
+        .then(data => {
+            /* Store Data */
+            console.log(data);
 
-        /* Retrive DOM nodes */
-        const ulEl = document.getElementById("emails-list");
+            /* Retrive DOM nodes */
+            const ulEl = document.getElementById("emails-list");
 
-        /* Create li element */
-        const li = document.createElement("li");
-        li.innerText = data.response;
+            /* Create li element */
+            const li = document.createElement("li");
+            li.innerText = data.response;
 
-        /* Append to ulEl */
-        ulEl.append(li);
-    })
-    .catch(error => {
-        console.log(error);
-    })
+            /* Append to ulEl */
+            ulEl.append(li);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+for (let i = 0; i < 10; i++) {
+    addEmail();
+}
